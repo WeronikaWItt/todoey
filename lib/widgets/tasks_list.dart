66d1cf5e4ec7ed92030ab6345935fb32d4421d6c
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2022-01-21 18:09:11 +0100

    added scaffold

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -14,44 +14,47 @@ class TasksList extends StatelessWidget {
           ),
         ),
       );
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<TaskData>(
-      builder: (context, taskData, child) {
-        return ListView.builder(
-          physics: const AlwaysScrollableScrollPhysics(),
-          itemBuilder: (context, index) {
-            final task = taskData.tasks[index];
-            return Slidable(
-              key: Key(task.id.toString()),
-              endActionPane: ActionPane(
-                motion: ScrollMotion(),
-                children: [
-                  SlidableAction(
-                    onPressed: (BuildContext context) => taskData.deleteTask(task),
-                    backgroundColor: Color(0xFFFE4A49),
-                    foregroundColor: Colors.white,
-                    icon: Icons.delete,
-                    label: 'Delete',
-                  ),
-                ],
-              ),
-              child: GestureDetector(
-                onTap: () => editTask(context, task),
-                child: TasksTile(
-                  taskTitle: task.taskTitle,
-                  taskDescription: task.description,
-                  isChecked: task.isDone,
-                  checkboxCallback: (checkboxCallback) {
-                    taskData.updateTaskStatus(task);
-                  },
+    return Scaffold(
+      body: Consumer<TaskData>(
+        builder: (context, taskData, child) {
+          return ListView.builder(
+            physics: const AlwaysScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              final task = taskData.tasks[index];
+              return Slidable(
+                key: Key(task.id.toString()),
+                endActionPane: ActionPane(
+                  motion: ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (BuildContext context) => taskData.deleteTask(task),
+                      backgroundColor: Color(0xFFFE4A49),
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      label: 'Delete',
+                    ),
+                  ],
                 ),
-              ),
-            );
-          },
-          itemCount: taskData.taskCount,
-        );
-      },
+                child: GestureDetector(
+                  onTap: () => editTask(context, task),
+                  child: TasksTile(
+                    taskTitle: task.taskTitle,
+                    taskDescription: task.description,
+                    isChecked: task.isDone,
+                    checkboxCallback: (checkboxCallback) {
+                      taskData.updateTaskStatus(task);
+                    },
+                  ),
+                ),
+              );
+            },
+            itemCount: taskData.taskCount,
+          );
+        },
+      ),
     );
   }
 }
