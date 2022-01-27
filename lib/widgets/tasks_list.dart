5d3bdae062d0c6e02059c82310f727a1edb11807
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2022-01-27 02:05:41 +0100

    deleted useless function

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do/constants.dart';
-import 'package:to_do/models/task.dart';
 import 'package:to_do/models/task_data.dart';
-import 'package:to_do/screens/edit_task.dart';
 import 'package:to_do/widgets/task_widget.dart';
 
 class TasksList extends StatelessWidget {
-  void editTask(BuildContext context, Task task) => Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => EditTask(task.id),
-        ),
-      );
-
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<TaskData>(context);
