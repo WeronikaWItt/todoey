6759c72f9d3eeb7a604279d28bef1bdfd1423bfd
WeronikaWitasik <40548953+WeronikaWitasik@users.noreply.github.com>
2022-02-11 12:03:48 +0100

    minor changes

--- lib/models/directory.dart
+++ lib/models/directory.dart
@@ -1,35 +1,35 @@
-// import 'dart:io';
-//
-// import 'package:path_provider/path_provider.dart';
-// import 'package:to_do/models/task.dart';
-//
-// class Directory {
-//   Future<String> get _localPath async {
-//     final directory = await getApplicationDocumentsDirectory();
-//
-//     return directory.path;
-//   }
-//
-//   Future<File> get getFilePath async {
-//     final path = await _localPath;
-//     return File('$path/tasks.txt');
-//   }
-//
-//   Future<File> writeTasks(Task task) async {
-//     final file = await getFilePath;
-//
-//     return file.writeAsString('$task');
-//   }
-//
-//   Future<String> readTasks() async {
-//     try {
-//       final file = await getFilePath;
-//
-//       final contents = await file.readAsString();
-//
-//       return contents;
-//     } catch (e) {
-//       return ' ';
-//     }
-//   }
-// }
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:to_do/models/task.dart';
+
+class Directory {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get getFilePath async {
+    final path = await _localPath;
+    return File('$path/tasks.txt');
+  }
+
+  Future<File> writeTasks(Task task) async {
+    final file = await getFilePath;
+
+    return file.writeAsString('$task');
+  }
+
+  Future<String> readTasks() async {
+    try {
+      final file = await getFilePath;
+
+      final contents = await file.readAsString();
+
+      return contents;
+    } catch (e) {
+      return ' ';
+    }
+  }
+}
